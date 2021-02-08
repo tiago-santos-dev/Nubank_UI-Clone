7cbe994620486890e3c189b82160a4b53d603e0b
TiagoRodrigues456 <trodrigues322@gmail.com>
2021-02-08 20:33:06 -0300

    TouchableOpacity in tabs

--- src/components/Tabs/index.js
+++ src/components/Tabs/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import {TouchableOpacity} from 'react-native';
 
 import {Container, TabsContainer, TabItem, TabText} from './styles';
 
@@ -8,26 +9,36 @@ export default function Tabs() {
   return (
     <Container>
       <TabsContainer>
-        <TabItem>
-          <Icon name="person-add" size={24} color="#FFF" />
-          <TabText>Indicar amigos</TabText>
-        </TabItem>
-        <TabItem>
-          <Icon name="chat-bubble-outline" size={24} color="#FFF" />
-          <TabText>Cobrar</TabText>
-        </TabItem>
-        <TabItem>
-          <Icon name="arrow-downward" size={24} color="#FFF" />
-          <TabText>Depositar</TabText>
-        </TabItem>
-        <TabItem>
-          <Icon name="arrow-upward" size={24} color="#FFF" />
-          <TabText>Transferir</TabText>
-        </TabItem>
-        <TabItem>
-          <Icon name="lock" size={24} color="#FFF" />
-          <TabText>Bloquear cartão</TabText>
-        </TabItem>
+        <TouchableOpacity>
+          <TabItem>
+            <Icon name="person-add" size={24} color="#FFF" />
+            <TabText>Indicar amigos</TabText>
+          </TabItem>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <TabItem>
+            <Icon name="chat-bubble-outline" size={24} color="#FFF" />
+            <TabText>Cobrar</TabText>
+          </TabItem>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <TabItem>
+            <Icon name="arrow-downward" size={24} color="#FFF" />
+            <TabText>Depositar</TabText>
+          </TabItem>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <TabItem>
+            <Icon name="arrow-upward" size={24} color="#FFF" />
+            <TabText>Transferir</TabText>
+          </TabItem>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <TabItem>
+            <Icon name="lock" size={24} color="#FFF" />
+            <TabText>Bloquear cartão</TabText>
+          </TabItem>
+        </TouchableOpacity>
       </TabsContainer>
     </Container>
   );
