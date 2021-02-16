60772a1681be9c34dd7b5cd213bee149650fda80
TiagoRodrigues456 <trodrigues322@gmail.com>
2021-02-15 23:49:07 -0300

    ADD Animations

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -21,6 +21,7 @@ import Tabs from '~/components/Tabs';
 import Menu from '~/components/Menu';
 
 export default function Main() {
+  let offset = 0;
   const translateY = new Animated.Value(0);
 
   const animatedEvent = Animated.event(
@@ -33,7 +34,32 @@ export default function Main() {
     ],
     {useNativeDriver: true},
   );
-  function onHandlerStateChanged(event) {}
+  function onHandlerStateChanged(event) {
+    if (event.nativeEvent.oldState === State.ACTIVE) {
+      let opened = false;
+      const {translationY} = event.nativeEvent;
+
+      offset += translationY;
+
+      if (translationY >= 100) {
+        opened = true;
+      } else {
+        translateY.setValue(offset);
+        translateY.setOffset(0);
+        offset = 0;
+      }
+
+      Animated.timing(translateY, {
+        toValue: opened ? 380 : 0,
+        duration: 200,
+        useNativeDriver: true,
+      }).start(() => {
+        offset = opened ? 380 : 0;
+        translateY.setOffset(offset);
+        translateY.setValue(0);
+      });
+    }
+  }
   return (
     <Container>
       <Header />
